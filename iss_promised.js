93f667edee7b80588dbb86f3f0a6bcf878e5e3fb
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-25 14:26:06 -0800

    linted

--- iss_promised.js
+++ iss_promised.js
@@ -1,17 +1,17 @@
 const request = require('request-promise-native');
 
-const fetchMyIP = function () {
+const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 };
 
-const fetchCoordsByIP = function (body) {
+const fetchCoordsByIP = function(body) {
   const ip = JSON.parse(body).ip;
   return request(`http://ipwho.is/${ip}`);
 };
 
-const fetchISSFlyOverTimes = function (body) {
+const fetchISSFlyOverTimes = function(body) {
   const { latitude, longitude } = JSON.parse(body);
-  return request(`https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`)
+  return request(`https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`);
 };
 
 module.exports = {
