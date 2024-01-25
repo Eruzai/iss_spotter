4f3360862de5ce19dea3960e5ea683f1c9a31337
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-25 09:59:00 -0800

    code for readability of fly overs

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
-nextISSTimesForMyLocation((error, passTimes) => {
+const printPassTimes = function(passTimes) { // function makes the passTimes returned from nextISSTimeForMyLocation more human readable
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => { // function returns ISS pass over times at my location
   if (error) {
     return console.log("It didn't work!", error);
   }
-  // success, print out the deets!
-  console.log(passTimes);
+  
+  printPassTimes(passTimes);
 });
