c0ebfd60b0c9fa45d55e31585681f0f2fce860fe
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-24 16:34:06 -0800

    returns proper geolocation and handles errors

--- iss.js
+++ iss.js
@@ -1,21 +1,44 @@
 const request = require('request');
 
-const fetchMyIP = function(callback) {
-  request('https://api.ipify.org?format=json', (error, response, body) => {
+// const fetchMyIP = function(callback) {
+//   request('https://api.ipify.org?format=json', (error, response, body) => {
+//     if (error) {
+//       callback(error, null);
+//       return;
+//     }
+//
+//     if (response.statusCode !== 200) { // if non-200 status, assume server error
+//       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+//       callback(Error(msg), null);
+//       return;
+//     }
+//     const data = JSON.parse(body).ip;
+    
+//     callback(null, data);
+//   });
+// };
+
+const fetchCoordsByIP = function(ip, callback) {
+  request('http://ipwho.is/' + ip, (error, response, body) => {
     if (error) {
       callback(error, null);
       return;
     }
-    // if non-200 status, assume server error
-    if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
+
+    if (JSON.parse(body).success === false) { // checks for failure message and if found sends a message as to what happened
+      callback(JSON.parse(body).message, null);
       return;
     }
-    const data = JSON.parse(body).ip;
+
+    const data = {};
+    data.latitude = JSON.parse(body).latitude.toString();
+    data.longitude = JSON.parse(body).longitude.toString();
     
     callback(null, data);
   });
 };
 
-module.exports = { fetchMyIP };
+module.exports = {
+  //fetchMyIP,
+  //fetchCoordsByIP
+};
