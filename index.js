6705b0f980c033f29ca8cd7439b9364a88adbbeb
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-25 09:40:57 -0800

    removed test code

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-// fetchCoordsByIP('23.16.38.56', (error, geoLocation) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-    
-//   console.log('It worked! Returned coords:' , geoLocation);
-// });
-
-fetchISSFlyOverTimes({ latitude: '48.4284207', longitude: '-123.3656444' }, (error, flyOverTimes) => {
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
   }
-    
-  console.log('It worked! Returned times:' , flyOverTimes);
+  // success, print out the deets!
+  console.log(passTimes);
 });
